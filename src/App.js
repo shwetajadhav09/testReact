f6f990d706e40a70ab0560815b5219eb6cdaa792
Shweta <scj99255@gmail.com>
2023-10-06 17:45:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import {HashRouter, Routes, Route} from "react-router-dom";
 import { Home,About, Blog } from './components/F2';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -10,11 +11,13 @@ function App() {
       {/* <F1 /> */}
       {/* <Nav /> */}
       <HashRouter>
+        <Nav/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact/>}/>
         </Routes>
       </HashRouter>
     </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom";
 
-function Nav(){
+function Nav()
+{
     return(
         <nav class="navbar">
-            {/*ehnus on left
-            home about blog on right*/}
             <Link to="/" class="navbar-brand">Ethnus</Link>
-            <div class = "nav">
-            <Link to="/home" class="nav-link">Home</Link>
-            <Link to="/about" class="nav-link">About</Link>
-            <Link to="/blog" class="nav-link">Blog</Link>
-            <Link to="#" class="nav-link">Contact</Link>
+            <div class="nav">
+                <Link to="/home" class="nav-link">Home</Link>
+                <Link to="/about" class="nav-link">About</Link>
+                <Link to="/blog" class="nav-link">Blog</Link>
+                <Link to="/contact" class="nav-link">Contact</Link>
             </div>
         </nav>
     )
 }
+
 export default Nav;
